acc3c3fa5e8c9d7aa7b6878551831399295d74fb
Isaque <isaque.tornquist@escola.pr.gov.br>
2026-04-03 00:40:15 -0300

    basico

---
+++ app/src/telas/Basico.js
@@ -0,0 +1,10 @@
+import React from "react";
+import{ Image, Text, View, StyleSheet, SafeAreaView, ScrollView} from "react-native";
+import me from '../../../assets/me.jpg';
+
+
+export default function Basico () {
+    return (
+      <Text></Text>
+    )
+}
